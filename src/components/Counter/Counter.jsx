0c379921186cac5eaad2ac0d3f971cf05e04cd62
Tomasz <mandalorian99@gmail.com>
2021-12-02 16:56:08 +0000

    counter done

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Counter.scss";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+
   return (
     <div className="counter">
       <h3 className="counter__name">Counter</h3>
-      <p className="counter__num">0</p>
-      <i className="far fa-minus-square"></i>
-      <i className="far fa-plus-square"></i>
+      <p className="counter__num">{counter}</p>
+      <i className="far fa-minus-square" onClick={handleDecrement}></i>
+      <i className="far fa-plus-square" onClick={handleIncrement}></i>
     </div>
   );
 };
